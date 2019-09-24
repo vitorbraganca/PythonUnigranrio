9a520bf21d1e9b282c023c497d39486f2a5fd5bd
Vitor Bragança <vitor_braganca@unigranrio.br>
2019-09-24 16:35:39 -0300

    Update 01-PrecoProdutos.py

--- PyQuest04/01-PrecoProdutos.py
+++ PyQuest04/01-PrecoProdutos.py
@@ -1,14 +1,29 @@
 # preco 10 produtos
-compras = [   ]
-soma = 0.0
-while True:
+
+def linha():
+    print('=' * 55)
+
+
+compras = []
+cont, soma = 0, 0.00
+
+while cont <= 9:
     produto = input('Nome do produto (digite FIM para encerrar): ')
     produto = produto.upper()
-    if produto == 'FIM':    break
+    if produto == 'FIM': break
     qntd = int(input('Quantidade: '))
     preco = float(input('Valor do produto: '))
     compras.append([produto, qntd, preco])
-    soma += preco*qntd
-print('Lista de compras: PRODUTO | QUANTIDADE | VALOR UNITARIO:')
-print(compras)
+    cont += 1
+soma = 0.0
+linha()
+print('                   Lista de compras')
+linha()
+for compra in compras:
+    print("%20s x%5d %5.2f %6.2f"
+          % (compra[0],  # Nome produto
+             compra[1], compra[2],  # Quantidade e preco unitario
+             compra[1] * compra[2]))  # Preco parcial
+    soma += compra[1] * compra[2]
+linha()
 print('Valor total dos produtos: {:.2f}'.format(soma))
