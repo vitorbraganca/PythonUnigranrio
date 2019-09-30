e3ff242fbbda45f5bb5b60a231d082b674bb7cba
Vitor Bragança <vitor_braganca@unigranrio.br>
2019-09-30 10:18:49 -0300

    Corrigindo

--- PyQuest04/10-MaterialRadioativo.py
+++ PyQuest04/10-MaterialRadioativo.py
@@ -1,8 +1,8 @@
 tempo = 0
-massa = float(input('Insira a massa do material: '))
-while massa > 0.50:
+massa = float(input('Insira a massa do material em gramas: '))
+while massa > 500:
     tempo += 1
     massa = massa / 2
 total = tempo * 50
 min = total / 60
-print('Levou {:.2f} segundos ou {:.2f} minutos.'.format(total, min))
+print('Levou {:.2f} segundos ou {:.2f} minutos ate que a massa final atingisse {:.2f} gramas'.format(total, min, massa))
