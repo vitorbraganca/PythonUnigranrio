8171814def83280c59b5be9a0c0afbbc389a9e04
Vitor Bragança <vitor_braganca@unigranrio.br>
2019-10-10 18:00:03 -0300

    Atualizacao
    Implementando compreensao de listas

--- Lista04/09-VetoresSoma.py
+++ Lista04/09-VetoresSoma.py
@@ -9,18 +9,11 @@ def linha(espaco, titulo, dupla):  # Criando uma funcao para impressao de linha 
 
 
 # Codigo principal
-num_a = []
-num_b = []
-num_c = []  # Criando vetores vaios para atribuicao
-
-for i in range(10):  # Leitura numeros vetor A
-    num_a.append(int(input(f'|VETOR A| Digite o numero na posicao {i+1}: ')))
+num_a = [int(input(f'Numero no vetor A, posicao {i+1}: ')) for i in range(10)]  # Criando lista para o vetor A utilizando compreensao de listas
 linha(30, ' ', 0)
-for c in range(10):  # Leitura numeros vetor A
-    num_b.append(int(input(f'|VETOR B| Digite o numero na posicao {c+1}: ')))
+num_b = [int(input(f'Numero no vetor B, posicao {c+1}: ')) for c in range(10)]  # Criando lista para o vetor B utilizando compreensao de listas
 linha(30, ' ', 0)
-for x in range(10):  # Soma numeros vetor A + vetor B
-    num_c.append(num_a[x]+num_b[x])  # atribuicao ao vetor c da soma do numero na posicao x (for) de cada vetor
-    print(f'|A: {num_a[x]}| + |B: {num_b[x]}| ||POSICAO {x+1}||')   # Impressao para controle de quais numeros e de quais posicoes
+num_c = [(num_a[x]+num_b[x]) for x in range(10)]  # Criando lista para o vetor C utilizando compreensao de listas
 linha(30, 'VETOR C:', 0)
-linha(30, num_c, 1)  # Impressao final do vetor C.
+print(num_c)  # Impressao final do vetor C.
+linha(30, ' ', 0)
