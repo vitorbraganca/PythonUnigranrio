35f1e7da96c6ff508f20a9f5fdb1a12e5caa71fa
Vitor Bragança <vitor_braganca@unigranrio.br>
2019-10-21 09:32:09 -0300

    Atualizacao
    Criacao de uma nova lista para relacao geral das pessoas.
    Antes, o programa nao guardava os demais dados, somente os maiores de 1.7m.

--- Lista04/01-Altura.py
+++ Lista04/01-Altura.py
@@ -1,16 +1,20 @@
-def linha(espaco, titulo): # Criando uma funcao para impressao de linha (manter o codigo limpo, visto que se repete algumas vezes)
+def linha(espaco, titulo):  # Criando uma funcao para impressao de linha (manter o codigo limpo)
     print('-='*espaco)
     if titulo != ' ':
         print(titulo)
         print('-='*espaco)
 
-relacao = []  # Criando um vetor vazio para relacao das pessoas com mais de 1.7m
-for i in range(50):
+
+relacao_menores = []  # Criando um vetor vazio para relacao das pessoas em geral
+relacao_maiores = []  # Criando um vetor vazio para relacao das pessoas com mais de 1.7m
+for i in range(3):
     nome = input('Digite o nome: ')  # Lendo o nome
     altura = float(input('Digite a altura em metros: '))  # Lendo altura
     if altura > 1.7:
-        relacao.append([nome, altura])
+        relacao_maiores.append([nome, altura])
+    else:
+        relacao_menores.append([nome, altura])
 linha(15, ' Relacao dos maiores de 1.7m')
-for i in relacao:
-    print(f'{i[0]} - {i[1]:.02f} metros')
-linha(15,' ')
+for i in relacao_maiores:
+    print(f'{i[0]:10} - {i[1]:.02f} metros')
+linha(15, ' ')
