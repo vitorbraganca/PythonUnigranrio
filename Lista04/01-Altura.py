5f057720eb4dd4b3d3a7323e0409f0e3545a83b7
Vitor Bragança <vitor_braganca@unigranrio.br>
2019-10-21 09:37:02 -0300

    Atualizacao
    Agora, apenas uma lista a ser usada mas todos os dados serão armazenados.

--- Lista04/01-Altura.py
+++ Lista04/01-Altura.py
@@ -5,16 +5,13 @@ def linha(espaco, titulo):  # Criando uma funcao para impressao de linha (manter
         print('-='*espaco)
 
 
-relacao_menores = []  # Criando um vetor vazio para relacao das pessoas em geral
-relacao_maiores = []  # Criando um vetor vazio para relacao das pessoas com mais de 1.7m
+relacao = []  # Criando um vetor vazio para relacao das pessoas
 for i in range(3):
     nome = input('Digite o nome: ')  # Lendo o nome
     altura = float(input('Digite a altura em metros: '))  # Lendo altura
-    if altura > 1.7:
-        relacao_maiores.append([nome, altura])
-    else:
-        relacao_menores.append([nome, altura])
+    relacao.append([nome, altura])
 linha(15, ' Relacao dos maiores de 1.7m')
-for i in relacao_maiores:
-    print(f'{i[0]:10} - {i[1]:.02f} metros')
+for i in relacao:
+    if i[1] > 1.7:
+        print(f'{i[0]:10} - {i[1]:.02f} metros')
 linha(15, ' ')
