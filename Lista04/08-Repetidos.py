d3eb3e7c9a21d7e42aa2c1dbb79c9ce418d762b6
Vitor Bragança <vitor_braganca@unigranrio.br>
2019-10-21 11:56:18 -0300

    Correcao

--- Lista04/08-Repetidos.py
+++ Lista04/08-Repetidos.py
@@ -9,13 +9,16 @@ def linha(espaco, titulo, dupla):  # Criando uma funcao para impressao de linha 
 
 
 # Codigo principal
+maior = 0
+listpos = []
 numeros = [int(input(f'Numero na posicao {i+1}: ')) for i in range(9)]  # Criando lista utilizando compreensao de listas
-numeros.sort()  # Organizando a lista na ordem
-maior = numeros[-1]  # Buscando o maior numero na ultima posicao do vetor
+for num in range(3):
+    if numeros[num] > maior:
+        maior = numeros[num]
 rpts = numeros.count(maior)  # Contando quantas vezes o maior se repete
-pos = numeros.index(maior)  # Buscando em qual posicao do vetor ele se repete
 linha(20, f'Maior numero: {maior}', 0)
-if rpts > 1:
-    linha(20, f'Se repete {rpts} vezes nas posicoes {(pos+1)} ate {pos+rpts}', 1)
-else:
-    linha(20, f'Se repete {rpts} vez na posicao {pos+1}', 1)
+for n in range(9):
+    if numeros[n] == maior:
+        listpos.append(n)
+linha(20, f'Se repete {rpts} vezes nas posicoes {listpos}', 1)
+    
